d9f14d45340e4b4b43fbc09a6f51eb1408caf4a0
vovaprint <vovaprint@mail.ru>
2019-02-25 07:27:09 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,30 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
+    public static void main(String[] args) {
+        hello("world");
+        hello("Sasha");
+
+        double l = 5;
+        System.out.println("Площадь квадрата " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
+    }
+
+
 
 }
