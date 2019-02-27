b9a63804517a39ea74a2d022ffccfda19207dfbd
vovaprint <vovaprint@mail.ru>
2019-02-27 19:42:23 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,30 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        hello("world");
-        hello("Sasha");
+  public static void main(String[] args) {
+    hello("world");
+    hello("Sasha");
 
-        double l = 5;
-        System.out.println("Площадь квадрата " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
-    }
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello " + somebody + "!");
-    }
-
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area (double a, double b) {
-        return a * b;
-    }
-
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
 
 
 }
